65f83e962d6588d1be2e7f4fc64da775aefa8316
gb-2312 <gb-2312@users.noreply.github.com>
2021-10-09 09:21:10 +0800

    modify: [macos & linux] move `InstallPackages` folder to `_install_packages` folder

--- _scripts/env.sh
+++ _scripts/env.sh
@@ -3,8 +3,8 @@
 
 # check cc65 compiler
 
-EXEC_PACKAGES_FOLDER=../InstallPackages/bin
-INSTALL_PACKAGES_FOLDER=../InstallPackages/packages
+EXEC_PACKAGES_FOLDER=../_install_packages/bin
+INSTALL_PACKAGES_FOLDER=../_install_packages/packages
 
 CC65_VERSION=2.19
 CC65_INSTALL_PACKAGE_VERSION=cc65-${CC65_VERSION}
