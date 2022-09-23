a3668f7a7abdc06fedb2a2d5046e39ba7515a5bc
Cyneprepou4uk <cyneprepou4uk@gmail.com>
2022-09-24 02:45:14 +0300

    Create assemble.sh

---
+++ Danny Sullivan's Indy Heat/assemble.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# transplant & edit: angel
+# date: Thu Oct  7 16:20:43 CST 2021
+# notice: Terminal only for MacOSX, Linux and MinGW
+# usage: sh assemble.sh
+# :: in order to enable "comparsion with previous version" and "restoring backup" functions
+# :: make 2 copies of <!rom_name.nes> and rename them as <!rom_name.old> and <!rom_name.bak>
+
+# :: disable unnecessary console messages if possible
+
+# return to parent-folder
+NES_OUTPUT_SIMPLE_NAME=dsih
+NES_OUTPUT_FILE_SIZE=131088
+
+# navigate to the directory
+BASH_EXEC_DIR=$(dirname "$0")
+# goto directory
+cd "${BASH_EXEC_DIR}"
+
+# common function(s)
+source ../_scripts/os_support.sh
+# environment function(s)
+source ../_scripts/env.sh
+
+# assemble-header function(s)
+source ../_scripts/assemble_header.sh
+# assemble-standard function(s) support
+source ../_scripts/assemble_standard.sh
+# assemble-footer function(s) support
+source ../_scripts/assemble_footer.sh
