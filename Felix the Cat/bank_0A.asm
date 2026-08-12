b84dc7471ab032eb93f175a58a0454d002bad911
Cyneprepou4uk <cyneprepou4uk@gmail.com>
2026-08-12 05:20:25 +0300

    ком

--- Felix the Cat/bank_0A.asm
+++ Felix the Cat/bank_0A.asm
@@ -1,6 +1,6 @@
 .segment "BANK_0A"
-.include "bank_ram.inc"
-.include "bank_val.inc"
+.include "copy_bank_ram.inc"
+.include "copy_bank_val.inc"
 .org $8000 ; for listing file
 ; 0x014010-0x01800F
 
